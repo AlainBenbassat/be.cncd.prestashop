acd4f467cae7199caecde12e1c1dd3c578a72761
Alain Benbassat <alain@businessandcode.eu>
2022-02-17 15:00:57 +0100

    don + achat

--- CRM/Prestashop/Contribution.php
+++ CRM/Prestashop/Contribution.php
@@ -2,6 +2,7 @@
 
 class CRM_Prestashop_Contribution {
   const FINANCIAL_TYPE_ACHAT = 11;
+  const FINANCIAL_TYPE_DON = 1;
   private $config;
 
   public function __construct($config) {
@@ -9,34 +10,83 @@ class CRM_Prestashop_Contribution {
   }
 
   public function create($contactId, $order) {
-    $contribId = $this->exists($contactId, $order->id);
-    if ($contribId === FALSE) {
-      $contribId = $this->createContribution($contactId, $order)['id'];
-    }
+    if (!$this->exists($contactId, $order->id)) {
+      $donationAmount = $this->getDonationAmount($order);
+      if ($donationAmount > 0) {
+        $contrib = $this->createDonationContribution($contactId, $order, $donationAmount);
+        $this->updateDeliveryNumber($contrib['id'], $order);
+      }
 
-    $this->updateProducts($contribId, $order);
-    $this->updateDeliveryNumber($contribId, $order);
+      $contrib = $this->createPurchaseContribution($contactId, $order, $donationAmount);
+      if ($contrib) {
+        $this->updateProducts($contrib['id'], $order, $donationAmount);
+        $this->updateDeliveryNumber($contrib['id'], $order);
+      }
+    }
   }
 
   private function exists($contactId, $orderId) {
-    $sql = "select max(id) from civicrm_contribution where source = 'boutique_o$orderId' and financial_type_id = " . self::FINANCIAL_TYPE_ACHAT;
+    $sql = "select count(id) from civicrm_contribution where source = 'boutique_o$orderId' and financial_type_id in (" . self::FINANCIAL_TYPE_ACHAT . ',' . self::FINANCIAL_TYPE_DON . ')';
     $id = CRM_Core_DAO::singleValueQuery($sql);
     if ($id) {
-      return $id;
+      return TRUE;
     }
     else {
       return FALSE;
     }
   }
 
-  private function createContribution($contactId, $order) {
+  private function getDonationAmount($order) {
+    $amount = 0;
+
+    foreach ($order->associations->order_rows as $row) {
+      if ($this->isDonationProduct($row->product_name)) {
+        $amount += $this->removeExtraZeroes($row->unit_price_tax_incl);
+      }
+    }
+
+    return $amount;
+  }
+
+  private function isDonationProduct($productName) {
+    if ($productName == "Don à l'Opération 11.11.11") {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  private function createPurchaseContribution($contactId, $order, $donationAmount) {
+    $totalAmount = $this->removeExtraZeroes($order->total_paid);
+    if ($totalAmount > $donationAmount) {
+      $params = [
+        'sequential' => 1,
+        'contact_id' => $contactId,
+        'receive_date' => $order->delivery_date,
+        'total_amount' => $totalAmount - $donationAmount,
+        'currency' => 'EUR',
+        'financial_type_id' => self::FINANCIAL_TYPE_ACHAT,
+        'payment_instrument_id' => 1, // credit card
+        'source' => 'boutique_o' . $order->id,
+      ];
+
+      $result = civicrm_api3('Contribution', 'create', $params);
+      return $result;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  private function createDonationContribution($contactId, $order, $donationAmount) {
     $params = [
       'sequential' => 1,
       'contact_id' => $contactId,
       'receive_date' => $order->delivery_date,
-      'total_amount' => $this->removeExtraZeroes($order->total_paid),
+      'total_amount' => $donationAmount,
       'currency' => 'EUR',
-      'financial_type_id' => self::FINANCIAL_TYPE_ACHAT,
+      'financial_type_id' => self::FINANCIAL_TYPE_DON,
       'payment_instrument_id' => 1, // credit card
       'source' => 'boutique_o' . $order->id,
     ];
@@ -45,11 +95,15 @@ class CRM_Prestashop_Contribution {
     return $result;
   }
 
-  private function updateProducts($contribId, $order) {
+  private function updateProducts($contribId, $order, $donationAmount) {
+    $orderContainsDonations = ($donationAmount > 0) ? TRUE : FALSE;
+
     $products = [];
     foreach ($order->associations->order_rows as $row) {
-      $products[] = $row->product_id;
-      $this->config->addPrestashopProduct($row->product_id, $row->product_name);
+      if ($orderContainsDonations == FALSE || !$this->isDonationProduct($row->product_name)) {
+        $products[] = $row->product_id;
+        $this->config->addPrestashopProduct($row->product_id, $row->product_name);
+      }
     }
 
     if (count($products) > 0) {
@@ -81,6 +135,6 @@ class CRM_Prestashop_Contribution {
       $newAmount .= '.00';
     }
 
-    return $newAmount;
+    return (float)$newAmount;
   }
 }
